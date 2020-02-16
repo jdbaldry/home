52e8a4a7d1e76a8f566f4e7230946a3afb0c2f4d
jdb <jack.baldry@oracle.com>
2020-02-16 11:58:30 +0000

    Construct empty dict type with make-dict

--- lisp/table.scm
+++ lisp/table.scm
@@ -1,14 +1,14 @@
 ; Implementation of a table like data structure in Scheme.
 
-; Construct a dictionary from a single key, value pair.
-(define (make-dict key value)
-    (list (cons key value)))
+; Construct a dictionary.
+(define (make-dict)
+    (cons "dict" ()))
 
 ; Add a key, value pair to the dictionary.
 ; TODO: overwrite existing keys since you can keep appending the same keys and slowing down get-dict.
 (define (put-dict dict key value)
-    (cond ((null? dict) (list (cons key value)))
-          (else (append dict (list (cons key value))))))
+    (cond ((null? (cdr dict)) (list (cons key value)))
+          (else (append (cdr dict) (list (cons key value))))))
 
 ; Get the value for a key in the dictionary.
 (define (get-dict dict key)
@@ -16,4 +16,4 @@
           ((equal? (caar dict) key) (cdar dict))
           (else (get-dict (cdr dict) key))))
 
-(define dict (put-dict (make-dict "test" "success") "orange" "green"))
+(define dict (put-dict (put-dict (make-dict) "orange" "green") "test" "success"))
