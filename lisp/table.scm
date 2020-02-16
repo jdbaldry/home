43b4b71994ed8fc2e46add8f21b7ee706f671270
jdb <jack.baldry@oracle.com>
2020-02-16 16:17:24 +0000

    Use typed dict in object methods

--- lisp/table.scm
+++ lisp/table.scm
@@ -7,13 +7,18 @@
 ; Add a key, value pair to the dictionary.
 ; TODO: overwrite existing keys since you can keep appending the same keys and slowing down get-dict.
 (define (put-dict dict key value)
-    (cond ((null? (cdr dict)) (list (cons key value)))
-          (else (append (cdr dict) (list (cons key value))))))
+    ; TODO: signal an actual error.
+    (if (is-dict? dict)
+            (cons "dict" (append (cdr dict) (list (cons key value))))
+            "wrong type"))
 
 ; Get the value for a key in the dictionary.
 (define (get-dict dict key)
-    (cond ((null? dict) "not found")
-          ((equal? (caar dict) key) (cdar dict))
-          (else (get-dict (cdr dict) key))))
+    ; TODO: signal an actual error.
+    (if (is-dict? dict)
+            (cond ((null? (cdr dict)) "not found")
+                ((equal? (caadr dict) key) (cdadr dict))
+                (else (get-dict (cddr dict) key)))
+            "wrong type"))
 
 (define dict (put-dict (put-dict (make-dict) "orange" "green") "test" "success"))
