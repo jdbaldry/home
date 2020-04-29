46572ceaf7e1f6a518de128090e0a426c0e69e43
jdb <mail@jdb.sh>
2020-04-29 19:23:27 +0100

    Remove broken MANPATH

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -3,7 +3,6 @@
   packageOverrides = pkgs: with pkgs; rec {
     userProfile = writeText "user-profile" ''
       export PATH=/run/wrappers/bin:/home/$USER/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin:$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/sbin:/bin:/usr/sbin:/usr/bin
-      export MANPATH=$HOME/.nix-profile/share/man:/nix/var/nix/profiles/default/share/man:/usr/share/man
     '';
     userPackages = pkgs.buildEnv {
       name = "user-packages";
