60d473f6f8565a9bf774e3fcb93fbb2cdd0ba396
jdb <mail@jdb.sh>
2020-04-30 09:51:53 +0100

    Use personal fork of powerline-go that ignores the kubernetes context namespace

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,6 +1,17 @@
 { 
   allowUnfree = true;
   packageOverrides = pkgs: with pkgs; rec {
+    powerline-go = pkgs.powerline-go.overrideAttrs (oldAttrs: rec {
+      version = "ignore-namespace";
+
+      src = fetchFromGitHub {
+        owner = "jdbaldry";
+        repo = "powerline-go";
+        rev = "731179742f730fc1b4ae5620ff8470d5ca11be70";
+        sha256 = "1rcidm4wpw5011c9ay3xdqywhfy0f9746czql8cawwxga2b553w9";
+      };
+    });
+
     userProfile = writeText "user-profile" ''
       export PATH=/run/wrappers/bin:/home/$USER/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin:$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/sbin:/bin:/usr/sbin:/usr/bin
     '';
