3a5ac344d815bac4562e2b3b64b406852e2844aa
jdb <mail@jdb.sh>
2020-04-30 20:58:17 +0100

    Use ~/.bashrc for setting PATH

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -12,9 +12,6 @@
       };
     });
 
-    userProfile = writeText "user-profile" ''
-      export PATH=/run/wrappers/bin:/home/$USER/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin:$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/sbin:/bin:/usr/sbin:/usr/bin
-    '';
     userPackages = pkgs.buildEnv {
       name = "user-packages";
       paths = [
