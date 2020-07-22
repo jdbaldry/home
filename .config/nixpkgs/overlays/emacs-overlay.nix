e8bd53218e12f8dd135f70eca8ba7bda754fcff8
Jack Baldry <jack.baldry@grafana.com>
2020-07-22 07:11:08 +0100

    Add untracked overlays
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

---
+++ .config/nixpkgs/overlays/emacs-overlay.nix
@@ -0,0 +1,4 @@
+# From https://github.com/nix-community/emacs-overlay.
+import (builtins.fetchTarball {
+  url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+})
---
+++ .config/nixpkgs/overlays/user-packages.nix
@@ -0,0 +1,66 @@
+# From: https://gist.github.com/LnL7/570349866bb69467d0caf5cb175faa74
+# To initialize the declarative management of userPackages:
+# $ nix-env -f '<nixpkgs>' -r -iA userPackages
+# Subsequently, to update userPackages:
+# $ nix-rebuild
+self: super:
+let
+  inherit (super) callPackage fetchFromGitHub;
+  jsonnet-mode = callPackage ../jsonnet-mode.nix { };
+in {
+  userPackages = super.userPackages or { } // {
+    # Packages to install
+    complete-alias = callPackage ../complete-alias.nix { };
+    direnv = self.direnv;
+    emacs = self.emacsWithPackages (epkgs:
+      with epkgs; [
+        company
+        dockerfile-mode
+        eglot
+        envrc
+        fira-code-mode
+        format-all
+        go-autocomplete
+        go-mode
+        gruber-darker-theme
+        jsonnet-mode
+        haskell-mode
+        hindent
+        magit
+        markdown-mode
+        nix-mode
+        nixpkgs-fmt
+        pinentry
+        smartparens
+        smex
+        terraform-mode
+        yaml-mode
+      ]);
+    diff-so-fancy = self.gitAndTools.diff-so-fancy;
+    lorri = self.lorri;
+    niv = self.niv;
+    powerline-go = super.powerline-go.overrideAttrs (old: rec {
+      version = "ignore-namespace";
+
+      src = fetchFromGitHub {
+        owner = "jdbaldry";
+        repo = "powerline-go";
+        rev = "21d2b69a10d29e09f0c836ac086b4cd689bd7fb3";
+        sha256 = "18cdkii3kx80hrk4l25blmpxl66nh27f5d6gy6pb8k15fcm76d75";
+      };
+    });
+    vlc = self.vlc;
+    zoom-us = self.zoom-us;
+
+    nix = self.nix;
+
+    nix-rebuild = super.writeScriptBin "nix-rebuild" ''
+      #!${super.stdenv.shell}
+      if ! command -v nix-env &>/dev/null; then
+        echo "warning: nix-env was not found in PATH, add nix to userPackages" >&2
+        PATH=${self.nix}/bin:$PATH
+      fi
+      exec nix-env -f '<nixpkgs>' -r -iA userPackages "$@"
+    '';
+  };
+}
