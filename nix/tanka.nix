fe5bc8cc4798857f0958c99986a0c5f1e766bd53
Jack Baldry <jack.baldry@grafana.com>
2020-07-22 11:42:04 +0100

    Add derivation for [Tanka](https://github.com/grafana/tanka)
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

---
+++ nix/tanka.nix
@@ -0,0 +1,20 @@
+{ stdenv, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "tanka";
+  version = "0.11.1";
+
+  src = fetchFromGitHub {
+    owner = "grafana";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "0hp10qgalglsdhh6z6v4azh2hsr89mdrv1g5lssfl5jyink409yd";
+  };
+
+  modSha256 = "0hgyibmxv4pkgwnw2ijnlga9mx2qj9liq529nvqm4j4hmj1xg4l5";
+
+  meta = with stdenv.lib; {
+    description = "Flexible, reusable and concise configuration for Kubernetes";
+    license = licenses.asl20;
+  };
+}
