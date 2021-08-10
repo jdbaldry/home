2ab3f6e9af79e83957a9934ce3a8209ee817c21f
Jack Baldry <jack.baldry@grafana.com>
2021-08-10 10:38:56 +0100

    chore: Update docsonnet
    Fixes non-deterministic behavior
    
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- nixpkgs/docsonnet/default.nix
+++ nixpkgs/docsonnet/default.nix
@@ -2,14 +2,14 @@
 with pkgs;
 buildGoModule rec {
   pname = "docsonnet";
-  version = "3e17576";
+  version = "v0.0.3";
 
   meta = with lib; { maintainers = with maintainers; [ jdbaldry ]; };
   src = fetchFromGitHub {
     owner = "jsonnet-libs";
     repo = pname;
     rev = version;
-    sha256 = "sha256-1tOPmCtQ/7S+nYByxHdVRmGSFBoHVjWSVvX+CAycWp8=";
+    sha256 = "sha256-ufiGEAn0PO48CO6esD2lKWxkiU2iCv25jAUqJq8TTTs=";
   };
   vendorSha256 = "sha256-24vkXvPsjp4CJJc7ajqAbK66ywXyIaBj/8r8FLYVXPE=";
 }
