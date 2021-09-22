ba0d8c44ca9a57eb99617992e825cd2cf0c2f22d
Jack Baldry <jack.baldry@grafana.com>
2021-09-22 12:01:49 +0100

    feat: Install jsonnet-lint
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- nixpkgs/overlay.nix
+++ nixpkgs/overlay.nix
@@ -1,4 +1,5 @@
 final: prev:
+
 with prev; {
   cue-lsp-server = callPackage ./cue-lsp-server { pkgs = prev; };
   complete-alias = callPackage ./complete-alias { pkgs = prev; };
@@ -7,6 +8,7 @@ with prev; {
   grabpl = callPackage ./grabpl { pkgs = prev; };
   jsonnetmod = callPackage ./jsonnetmod { pkgs = prev; };
   jsonnet-bundler = callPackage ./jsonnet-bundler { pkgs = prev; };
+  jsonnet-lint = callPackage ./jsonnet-lint { pkgs = prev; };
   jsonnet-mode = callPackage ./jsonnet-mode { pkgs = prev; };
   misspell = callPackage ./misspell { pkgs = prev; };
   mixtool = callPackage ./mixtool { pkgs = prev; };
