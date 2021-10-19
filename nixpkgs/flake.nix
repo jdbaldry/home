cc7815913f2886a14d05a9514bb71c7cd3f5b647
Jack Baldry <jack.baldry@grafana.com>
2021-10-19 07:31:23 +0100

    refactor: η reduction of overlay output
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- nixpkgs/flake.nix
+++ nixpkgs/flake.nix
@@ -2,6 +2,6 @@
   description = "A flake for personal Nix packages";
 
   outputs = { self }: {
-    overlay = final: prev: import ./overlay.nix final prev;
+    overlay = import ./overlay.nix;
   };
 }
