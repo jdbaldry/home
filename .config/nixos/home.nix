5616dbb4ed024105626a6c2157cb1c51244bbaf5
Jack Baldry <jack.baldry@grafana.com>
2022-03-19 14:35:03 +0000

    Enable mbsync.timer
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- .config/nixos/home.nix
+++ .config/nixos/home.nix
@@ -1,3 +1,7 @@
+{ pkgs ? import <nixpkgs>, ... }:
+
+with pkgs;
+
 {
   accounts.email.accounts = {
     fastmail = {
@@ -6,12 +10,13 @@
       address = "jdbaldry@fastmail.com";
       imap.host = "imap.fastmail.com";
       smtp.host = "smtp.fastmail.com";
-      passwordCommand = "pass show smtp.fastmail.com:465/jdbaldry@fastmail.com";
+      passwordCommand = "${pass}/bin/pass show smtp.fastmail.com:465/jdbaldry@fastmail.com";
       userName = "jdbaldry@fastmail.com";
 
       mbsync = {
         enable = true;
-        create = "maildir";
+
+        create = "both";
       };
       msmtp.enable = true;
       mu.enable = true;
@@ -30,12 +35,16 @@
       address = "jdbaldry@gmail.com";
       imap.host = "imap.gmail.com";
       smtp.host = "smtp.gmail.com";
-      passwordCommand = "pass show smtp.gmail.com:465/jdbaldry@gmail.com";
+      passwordCommand = "${pass}/bin/pass show smtp.gmail.com:465/jdbaldry@gmail.com";
       userName = "jdbaldry@gmail.com";
 
       mbsync = {
         enable = true;
-        create = "maildir";
+
+        create = "both";
+        extraConfig.account = {
+          PipelineDepth = 1;
+        };
       };
       msmtp.enable = true;
       mu.enable = true;
@@ -54,12 +63,16 @@
       address = "jack.baldry@grafana.com";
       imap.host = "imap.gmail.com";
       smtp.host = "smtp.gmail.com";
-      passwordCommand = "pass show smtp.gmail.com:465/jack.baldry@grafana.com";
+      passwordCommand = "${pass}/bin/pass show smtp.gmail.com:465/jack.baldry@grafana.com";
       userName = "jack.baldry@grafana.com";
 
       mbsync = {
         enable = true;
-        create = "maildir";
+
+        create = "both";
+        extraConfig.account = {
+          PipelineDepth = 1;
+        };
       };
       msmtp.enable = true;
       mu.enable = true;
