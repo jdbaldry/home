1a539f5dbcb4cd6845aca1fed4a734f4ee8dd9e8
Jack Baldry <jack.baldry@grafana.com>
2022-03-19 14:35:28 +0000

    Update mixtool derivation
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- nixpkgs/mixtool/default.nix
+++ nixpkgs/mixtool/default.nix
@@ -1,21 +1,22 @@
 { pkgs ? import <nixpkgs> }:
+
 with pkgs;
 buildGoModule rec {
   pname = "monitoring-mixins";
-  version = "ae18e31";
+  version = "29fe1d8";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = "mixtool";
     rev = "${version}";
-    sha256 = "sha256-ZRvgwyEKBYsbff/MnUVRwMJAB7kco4D9Lfs9yQiqofI=";
+    sha256 = "sha256-8KWZCVQaPxDPQItnmIK3I5yvrdYeZfRizYK2V3sgdoY=";
   };
 
   doCheck = false;
   subPackages = [ "cmd/mixtool" ];
-  vendorSha256 = "sha256-ahixB0pjBRJbCZp/r8MmbowTRLKySL1dfVxC9Z75wnw=";
+  vendorSha256 = "sha256-pshD0Rb+vkkYVcJRQ98TCWZn2kO99u+mbrA9npVlrrs=";
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Helper for easily working with Jsonnet mixins";
     license = licenses.asl20;
   };
