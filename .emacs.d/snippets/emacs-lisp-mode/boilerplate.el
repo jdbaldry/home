30bc71030c33b7f7b174532f0a9ffb6405401b30
Jack Baldry <jack.baldry@grafana.com>
2022-04-03 18:10:26 +0100

    Add YAS snippets
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

---
+++ .emacs.d/snippets/emacs-lisp-mode/boilerplate.el
@@ -0,0 +1,16 @@
+# -*- mode: snippet -*-
+# name: boilerplate
+# key: boilerplate
+# --
+;;; ${1:package} --- ${2:summary}
+;;
+
+;;; Commentary:
+;;
+
+;;; Code:
+;;
+$0
+
+(provide '$1)
+;;; $1.el ends here
